94a8af9e77d1d816f8bc874ea35f943d2f9c4071
Sidharth Shanker <sshanker220@gmail.com>
2012-08-04 16:18:48 -0700

    First commit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collabdoc::Application.config.secret_token = 'da4ef2177e8845bbd0e2d237a0f1b330956072817d3c7ff646d6532bb143d22e8bbe3309da55d23e12cafa68c980adc64bcb4b983bd1a673aa3b3384cdfb68c4'
